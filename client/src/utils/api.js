6d1f9943067a0374352794bd4b63b5b52d5e7e48
Abdul-barr Shonibare <45232434+cybarry@users.noreply.github.com>
2024-05-08 11:14:38 +0100

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://real-agent-ap-pv1.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
