9e8b613d13a5b27ca3d81e2ccdfaa1aa426dc1f4
Abdul-barr Shonibare <45232434+cybarry@users.noreply.github.com>
2024-05-08 11:27:16 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-nuw8jlau1emkr48r.us.auth0.com"
       clientId="zodxhMSKmXRO7HaRjtIPkLZsTjJAUnK8"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173"
+        redirect_uri: "https://real-agent-ap-pv1-client.vercel.app"
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
